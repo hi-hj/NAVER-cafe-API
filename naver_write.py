d9f13b18b59c2a4cc9879d214d55e9f1c9c84133
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2021-01-02 14:56:15 +0900

    네이버 카페 글쓰기 API

--- naver_write.py
+++ naver_write.py
@@ -11,20 +11,25 @@ BOARD_NAME = 'YB_BOARD_TEST'
 conn = sqlite3.connect(DB)
 cur = conn.cursor()
 
-token = "AAAAOqlO4rT5yZyRfS7wabDSJv30uARtgDjVHNXMPBu4TobuIKkTHhYENVPMJ7706iXQJNLrW_tC8CbkKtmQyEpXH3o"
+# Set token & config
+token = "" # get_token.py에서 확인한 access_token
 header = "Bearer " + token # Bearer 다음에 공백 추가
-clubid = "30067107" # 카페의 고유 ID값
-menuid = "10" # (상품게시판은 입력 불가)
+clubid = "******" # 카페의 고유 ID값
+menuid = "**" # (상품게시판은 입력 불가)
 url = "https://openapi.naver.com/v1/cafe/" + clubid + "/menu/" + menuid + "/articles"
 
+# NAVER API func
 def naver_upload():
     global row
     global header
     global clubid
     global menuid
     global url
+
     print(row[0])
-    subject = urllib.parse.quote('['+row[3].strip()[0:3]+'] ' + row[1])
+    
+    #subject = urllib.parse.quote('['+row[3].strip()[0:3]+'] ' + row[1])
+    subject = urllib.parse.quote('['+row[3]+'] ' + row[1])
     content = urllib.parse.quote(row[4].replace('\"', '\''))
     data = urlencode({'subject': subject, 'content': content}).encode()
     request = urllib.request.Request(url, data=data)
@@ -36,9 +41,8 @@ def naver_upload():
         print(response_body.decode('utf-8'))
     else:
         print("Error Code:" + rescode)
-for row in cur.execute("SELECT * FROM YB_BOARD_TEST ORDER BY 1 DESC LIMIT 200 OFFSET 1422"):
-    # print(row[1])
-    # print(row[3])
-    # print(row[4].replace('\"', '\''))
+
+# DB 데이터 -> 네이버 카페
+for row in cur.execute("SELECT * FROM YB_BOARD_TEST ORDER BY 1 DESC LIMIT 200 OFFSET 2622"):
     naver_upload()
-    time.sleep(10)
+    time.sleep(10) # 딜레이 없을 시, 403 에러 발생
